d6ad0d6b543238f6092d383faaa82edb78224df7
macarenaromero <macarenasromero@gmail.com>
2022-06-23 15:11:28 -0300

    Adición del 404

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080;
 const productsRoute = require("./routes/productsRoute");
 const cartRoute = require("./routes/cartRoute");
+const notFoundRoute = require("./routes/main");
 
 
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.use("/api/productos", productsRoute);
 app.use("/api/carrito", cartRoute);
+app.use("/*", notFoundRoute);
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`);
